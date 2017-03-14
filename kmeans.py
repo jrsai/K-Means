8b020e5b521283c284cbded2c78ee60791f7993e
Sai <jrsai@users.noreply.github.com>
2017-03-13 21:30:01 -0400

    kmeans.py commented version

--- kmeans.py
+++ kmeans.py
@@ -5,12 +5,12 @@ from matplotlib import style
 style.use('ggplot')
 
 #reading the file
-df = pd.read_csv('exercise-6.csv',delimiter=',',skipinitialspace=True)
+df = pd.read_csv('exercise-1.csv',delimiter=',',skipinitialspace=True)
 # making a array list to hold the x values (first column) from the csv file
 x = []
 y = []
-x.extend(np.around(df['x'],decimals=1)) # assigning the first column values to the array
-y.extend(np.around(df['y'],decimals=1)) # assigning the second column values to the array
+x.extend(np.around(df['sepal_width'],decimals=1)) # assigning the first column values to the array
+y.extend(np.around(df['petal_width'],decimals=1)) # assigning the second column values to the array
 # formatting the array so every element in it is to 2 decimal places
 formattedX=['%.2f' % elem for elem in x]
 formattedY=['%.2f' % elem for elem in y]
@@ -18,6 +18,7 @@ formattedY=['%.2f' % elem for elem in y]
 # the matrix is the new array that we pass to the k-means class
 newX = df.as_matrix()
 
+#plot original to show without clusters
 plt.scatter(formattedX,formattedY)
 plt.title('Original Unclustered Data')
 plt.xlabel('x')
@@ -28,68 +29,71 @@ colors = 10*["g","r","c","b","k"]
 
 class K_Means:
     # toleraence is the percentage of how much the centroid is going to move, max_iter is the max iterations the program will make
-    def __init__(self, k = 3, tolerance = 0.001, max_iter = 300):
+    def __init__(self, k = 2, tolerance = 0.001, max_iter = 300):
         self.k = k
         self.tolerance = tolerance
         self.max_iter = max_iter
 
         #keeps a constant number of centroids to move
-    def fit(self, data):
+    def amnt_of_centroids(self, data):
         self.centroids = {} #empty dictionary to store centroids
 
-        #iterating through data, takes the first two data points as centroids to use
+        # iterating through data, takes the first two data points as centroids to use
         for i in range(self.k):
             self.centroids[i] = data[i]
 
-        #empties the centroids every time the centroid moves, so empty for every iteration
+        # empties the centroids every time the centroid moves, so empty for every iteration
         for i in range(self.max_iter):
             self.classifications = {} #empty dictionary to store new centroids
 
-            #iterating through centroids keys to equal empty list
-            for i in range (self.k):
-                self.classifications[i] = [] #empty list of feature set values
+            # iterating through centroids keys to equal empty list
+            for j in range (self.k):
+                self.classifications[j] = [] #empty list of data set values
 
-            #populating the empty list created above/ giving it values
-            for featureset in data:
+            # populating the empty list created above/ giving it values
+            for dataset in data:
                 # calculates the distance between the data and the centroid
-                distances = [np.linalg.norm(featureset - self.centroids[centroid]) for centroid in self.centroids] #creating a list with k # of centroids
-                classification = distances.index(min(distances)) #gets the min distance centroid and assign it to list.
-                self.classifications[classification].append(featureset) #adds the new centroid found to the dictionary
+                distances = [np.linalg.norm(dataset - self.centroids[centroid]) for centroid in self.centroids] #creating a list with k # of centroids
+                key = distances.index(min(distances)) #returns the lowest index in the list, so the first element can be used
+                self.classifications[key].append(dataset) #adds the new centroid found to the dictionary
 
             # comparing the two centroids, to see how much they have changed
             prev_centroids = dict(self.centroids)
 
-            for classification in self.classifications:
+            for key in self.classifications:
                 # takes the mean of each cluster to calculate new centroid of the cluster
-                self.centroids[classification] = np.average(self.classifications[classification], axis = 0)
+                self.centroids[key] = np.average(self.classifications[key], axis = 0)
 
             optimized = True
 
             # if any of the centroids in their movement move more than the tolerance (0.001), we say we are not optimized
-            for c in self.centroids:
-                original_centroid = prev_centroids[c]
-                current_centroid = self.centroids[c]
+            for i in self.centroids:
+                original_centroid = prev_centroids[i]
+                current_centroid = self.centroids[i]
 
                 if np.sum((current_centroid - original_centroid) / original_centroid * 100.0) > self.tolerance:
                     optimized = False
+
                 # this will break the for loop and stop us from running every single maximum iteration
                 # If this is not the case it will continue through the for loop until it reaches the maximum iteration and at that time whatever the centroid is. This is our final centroid
                 if optimized:
                     break
 
-clf = K_Means()
-clf.fit(newX)
+k = K_Means()
+k.amnt_of_centroids(newX)
+
 # plots the centroids
-for centroid in clf.centroids:
-    plt.scatter(clf.centroids[centroid][0] , clf.centroids[centroid][1], marker = "*", color = "k", s = 150)
+for centroid in k.centroids:
+    plt.scatter(k.centroids[centroid][0] , k.centroids[centroid][1], marker = "*", color = "k", s = 150)
 
 # Classifies which color to use for each clusters
-for classification in clf.classifications:
-    color = colors[classification]
+for key in k.classifications:
+    color = colors[key]
+    # plots the clusters
+    for dataset in k.classifications[key]:
+        plt.scatter(dataset[0], dataset[1], marker = "o", color = color, linewidths = 2)
 
-    for featureset in clf.classifications[classification]:
-        plt.scatter(featureset[0], featureset[1], marker = "o", color = color, linewidths = 2)
-
+# plots the clustered cata
 plt.title('Clustered data')
 plt.xlabel('x')
 plt.ylabel('y')
